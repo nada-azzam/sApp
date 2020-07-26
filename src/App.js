1b2acef98f42cba2933b98fe448e213dee678ee9
NadaAzzzam <nadaazzam467@gmail.com>
2020-07-26 17:19:42 +0200

    create sApp Landing Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <i class="fas fa-cog"></i>
         </p>
         <a
           className="App-link"
